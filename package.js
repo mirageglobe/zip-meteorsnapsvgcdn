535e04381f40f1f7c3364335f59917610d100707
Jimmy MG Lim <mirageglobe@gmail.com>
2014-10-25 14:18:32 +0100

    fixed client only loading

--- package.js
+++ package.js
@@ -1,17 +1,17 @@
 Package.describe({
   name: 'mirageglobe:snapsvgcdn',
   summary: 'CDN based Snapsvg for Meteor',
-  version: '1.0.0',
+  version: '1.0.1',
   git: 'https://github.com/mirageglobe/meteorsnapsvgcdn'
 });
 
 Package.onUse(function(api) {
-  api.versionsFrom('0.9.0');
-  api.addFiles('mirageglobe:snapsvgcdn.js');
+  api.versionsFrom('0.9.4');
+  api.addFiles('mirageglobe:snapsvgcdn.js','client');
 });
 
 Package.onTest(function(api) {
   api.use('tinytest');
   api.use('mirageglobe:snapsvgcdn');
-  api.addFiles('mirageglobe:snapsvgcdn-tests.js');
+  api.addFiles('mirageglobe:snapsvgcdn-tests.js','client');
 });
