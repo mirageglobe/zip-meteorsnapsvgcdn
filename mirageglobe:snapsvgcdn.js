98aaf5d7464965ce9c52f1985ac4d5dbdf3262f0
Jimmy MG Lim <mirageglobe@users.noreply.github.com>
2017-08-15 23:55:35 +0100

    Updated to version 0.5.1

--- mirageglobe:snapsvgcdn.js
+++ mirageglobe:snapsvgcdn.js
@@ -1,5 +1,5 @@
 var script = document.createElement('script');
 
-script.src = "//cdn.jsdelivr.net/snap.svg/0.4.1/snap.svg-min.js";
+script.src = "//cdn.jsdelivr.net/snap.svg/0.5.1/snap.svg-min.js";
 
 document.getElementsByTagName('head')[0].appendChild(script);
